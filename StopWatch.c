5a002dc04c1c9c2146ce2313fbeb26d682786dc9
Mykyta <129153037+Niekit56@users.noreply.github.com>
2024-02-25 19:32:35 +0100

    Add files via upload

--- StopWatch.c
+++ StopWatch.c
@@ -100,6 +100,30 @@ void drawDigit(int digit, char buffer[][100]) {
     }
 }
 
+//Function for reading a txt file
+void printFileContent(const char *filename) {
+    
+    FILE *file;
+    char ch;
+
+    // Open the file for reading
+    file = fopen(filename, "r");
+
+    // Check if the file was opened successfully
+    if (file == NULL) {
+        printf("Failed to open file %s.\n", filename);
+        return;
+    }
+   
+    // Read characters from the file and output them to the console
+        while ((ch = fgetc(file)) != EOF) {
+            printf("%c", ch);
+        }
+
+    fclose(file);
+    
+}
+
 int main() {
 
     int minutes = 0;
@@ -107,6 +131,15 @@ int main() {
 
     char digitBuffers[5][7][100]; // Buffers to store each digit
     while(TRUE) {
+
+        system("clear");
+
+        // Use ANSI control characters to change the text color
+        printf("\033[0;32m"); // \033 - escape sequence, [0;32m - code for green color 
+
+        //Print data from the file
+        printFileContent("pattern.txt");
+
         // Draw first digit of minutes
         drawDigit(minutes / 10, digitBuffers[0]);
         // Draw second digit of minutes
@@ -118,18 +151,14 @@ int main() {
         // Draw second digit of seconds
         drawDigit(seconds % 10, digitBuffers[4]);
 
-        // Use ANSI control characters to change the text color
-        printf("\033[0;32m"); // \033 - escape sequence, [0;32m - code for green color       
-
         // Print the buffer
         for (int i = 0; i < 7; i++) {
-            printf("%s  %s  %s  %s  %s\n", digitBuffers[0][i], digitBuffers[1][i], digitBuffers[2][0], digitBuffers[3][i], digitBuffers[4][i]);
+            printf("\t\t%s  \t%s  \t%s  \t%s  \t%s\n", digitBuffers[0][i], digitBuffers[1][i], digitBuffers[2][0], digitBuffers[3][i], digitBuffers[4][i]);
         }
 
         fflush(stdout);
         usleep(1000000); // 1000000 microseconds = 1 second
-        system("clear");
-
+        
         //simple logic for changing symbols
         seconds++;
         if (seconds == 60) {
@@ -140,5 +169,6 @@ int main() {
             }
         }
     }
+    
     return 0;
 }
